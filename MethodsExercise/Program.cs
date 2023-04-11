84e9c363c33c030eec5c465ea3e32990f868870c
AlissaStegall <alissaistegall@gmail.com>
2023-04-11 15:52:02 -0700

    began user questions

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, what is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName}. what is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is a great color! What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Wonderful! Lastly, What is your favorite band?");
+            var band = Console.ReadLine();
+
+
         }
     }
 }
