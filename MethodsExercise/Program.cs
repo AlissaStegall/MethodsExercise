322e18b29fb96358416f76707e85fba0c53bcb69
AlissaStegall <alissaistegall@gmail.com>
2023-04-11 16:00:15 -0700

    finished profile config

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -16,7 +16,13 @@
             Console.WriteLine("Wonderful! Lastly, What is your favorite band?");
             var band = Console.ReadLine();
 
+            Console.WriteLine($"Thanks, {userName}. Here is your profile.");
+            Console.WriteLine("--------------------");
 
+            Console.WriteLine($"Name: {userName}");
+            Console.WriteLine($"Favorite color: {color}");
+            Console.WriteLine($"Favorite animal: {animal}");
+            Console.WriteLine($"Favorite band: {band}");
         }
     }
 }
